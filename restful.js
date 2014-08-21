813dfec389f8edef3ee996365cb72682563e3ab2
David Njuguna <dnjuguna@gmail.com>
2014-08-21 21:07:35 +0300

    initial

---
+++ restful.js
@@ -0,0 +1,77 @@
+"use strict";
+module.exports = {
+	"get": function(context, then){
+		try{
+			var sandbox = { context : context };
+			sandbox.operation = "find";
+			var model = new (require(__dirname + "/model.js"));
+			model.init(sandbox);
+            if(!model.allow)
+                sandbox.context.statusCode(401);
+			if(!model.allow)
+				throw new Error("You are not allowed access to this resource.");
+			if(sandbox._id)
+				model.query._id = sandbox._id;
+			model.collection.find(model.query).sort({creation_time: -1}).toArray(function(error, items){
+				if(!error)
+					return sandbox.data(items).end();
+				sandbox.context.log(2, error.stack);
+				throw new Error("Error while trying query collection.");
+			})
+		}catch(error){
+			sandbox.context.log(2, error.stack);
+			sandbox.data({error: error.stack});
+			sandbox.end();
+		}
+	},
+	"post": function(sandbox){
+		try{
+			var sandbox = {context : context};
+			var model = new (require(__dirname + "/model.js"));
+			model.init(sandbox);
+            if(!model.allow)
+                sandbox.context.statusCode(401);
+            if(!model.allow)
+                throw new Error("User is not allowed write access to this resource.");
+            if(sandbox._id)
+                model.query._id = sandbox._id;
+			model[sandbox.operation](function(error, items){
+				if(error)
+					throw new Error(error);
+				return sandbox.data(items).end();
+			});
+		}catch(error){
+			sandbox.context.log(2, error.toString());
+			sandbox.context.statusCode(503);
+			sandbox.data({error: error.toString()});
+			sandbox.end();
+		}
+	},
+	"delete": function(sandbox){
+		try{
+			var sandbox = {context : context};
+			sandbox.operation = "remove";
+			var model = new (require(__dirname + "/model.js"));
+			model.init(sandbox);
+            if(!model.allow)
+                sandbox.context.statusCode(401);
+            if(!model.allow)
+                throw new Error("You are not allowed to delete this resource.");
+            if(sandbox._id)
+                model.query._id = sandbox._id;
+			model.collection.remove(query, function(error, items){
+				if(!error)
+					return sandbox.data(items).end();
+				sandbox.context.statusCode(503);
+				return sandbox.data({error: error.toString()}).end();
+			});
+		}catch(error){
+			sandbox.context.log(2, error.toString());
+			sandbox.data({error: error.toString()});
+			sandbox.end();
+		}
+	},
+	"put": function(sandbox){
+		//TODO
+	}
+};
