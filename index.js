58e3ea042dff128af2cf83ddd224a21b8ebbe808
David Njuguna <dnjuguna@gmail.com>
2014-08-21 23:23:50 +0300

    created a handlers hash

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+module.exports = {
+	restful: require(__dirname + "/restful.js")
+};
--- restful.js
+++ restful.js
@@ -14,14 +14,14 @@ module.exports = {
 				model.query._id = sandbox._id;
 			model.collection.find(model.query).sort({creation_time: -1}).toArray(then);
 		}catch(error){
-			then(error, null);
-			context.log(1, error.stack);
+			then(error.toString(), null);
+			context.log(2, error.stack);
 		}
 	},
-	"post": function(sandbox){
+	"post": function(context, then){
 		try{
 			var sandbox = {context : context};
-			var model = new (require(__dirname + "/model.js"));
+			var model = new (require(__dirname + "/lib/model.js"));
 			model.init(sandbox);
             if(!model.allow)
                 sandbox.context.statusCode(401);
@@ -29,23 +29,17 @@ module.exports = {
                 throw new Error("User is not allowed write access to this resource.");
             if(sandbox._id)
                 model.query._id = sandbox._id;
-			model[sandbox.operation](function(error, items){
-				if(error)
-					throw new Error(error);
-				return sandbox.data(items).end();
-			});
+			model[sandbox.operation](then);
 		}catch(error){
-			sandbox.context.log(2, error.toString());
-			sandbox.context.statusCode(503);
-			sandbox.data({error: error.toString()});
-			sandbox.end();
+			then(error.toString(), null);
+			sandbox.context.log(2, error.stack);
 		}
 	},
-	"delete": function(sandbox){
+	"delete": function(context, then){
 		try{
 			var sandbox = {context : context};
 			sandbox.operation = "remove";
-			var model = new (require(__dirname + "/model.js"));
+			var model = new (require(__dirname + "/lib/model.js"));
 			model.init(sandbox);
             if(!model.allow)
                 sandbox.context.statusCode(401);
@@ -53,16 +47,10 @@ module.exports = {
                 throw new Error("You are not allowed to delete this resource.");
             if(sandbox._id)
                 model.query._id = sandbox._id;
-			model.collection.remove(query, function(error, items){
-				if(!error)
-					return sandbox.data(items).end();
-				sandbox.context.statusCode(503);
-				return sandbox.data({error: error.toString()}).end();
-			});
+			model.collection.remove(query, then);
 		}catch(error){
-			sandbox.context.log(2, error.toString());
-			sandbox.data({error: error.toString()});
-			sandbox.end();
+            then(error.toString(), null);
+            sandbox.context.log(2, error.stack);
 		}
 	},
 	"put": function(sandbox){
